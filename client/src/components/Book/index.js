d957af285852fceeaadeab9ec4c4581e620d825f
chrislewisjones <47401704+chrislewisjones@users.noreply.github.com>
2019-04-29 23:29:30 -0500

    got search page to render

--- client/src/components/Book/index.js
+++ client/src/components/Book/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./style.css";
+
 import { ListItem } from "../List";
 
 const Book = ({
---
+++ client/src/components/SearchForm/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+function SearchForm({ query, handleInputChange, handleFormSubmit }) {
+  return (
+    <form className="form-inline" role="form">
+      <div className="form-group mx-sm-3 mb-2">
+        <label htmlFor="Title" className="sr-only">
+          Search Book Title
+        </label>
+        <input
+          className="form-control heading-subtitle "
+          id="Title"
+          type="text"
+          value={query}
+          placeholder="Book Title..."
+          name="q"
+          onChange={handleInputChange}
+          size="55"
+          required
+        />
+      </div>
+      <button
+        onClick={handleFormSubmit}
+        type="submit"
+        className="btn btn-lg search-button heading-subtitle"
+      >
+        Search
+      </button>
+    </form>
+  );
+}
+
+export default SearchForm;
